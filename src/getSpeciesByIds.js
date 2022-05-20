c3c6aed7496d001ca13e69e7a330fe525c27ac27
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-05-20 19:36:01 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
-  // seu código aqui
+//
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+    /* aqui estou verificando se tem algum parametro no meu ids de entrada, se ele for vazio retorna um array vazio [] */
+  }
+  return ids.map((mapId) => data.species.find(({ id }) => id === mapId));
+  /* se tem algum id de parametro no meu ids de entrada, ele me retorna um .map somente com o species, depois usei um .find pra me retornar os ids de cada parametro que passar */
 }
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
