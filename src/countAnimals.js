fdcb46bac9230023477edea387746042c99a2b80
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-05-23 20:13:12 -0300

    adc observação

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -21,6 +21,7 @@ console.log(countAnimals({ specie: 'giraffes' }));
 
 module.exports = countAnimals;
 
+/* Primeiro verifiquei se o parametro era undefined ou não, caso fosse, ele passaria por um reduce que ira retornar um obj com os animais e as especies. Para a resolução desses exercícios ultilizei as fontes abaixo: */
 // https://stackoverflow.com/questions/43170806/merge-multiple-arrays-based-on-their-index-using-javascript
 // https://www.w3schools.com/jsref/jsref_reduce.asp
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
