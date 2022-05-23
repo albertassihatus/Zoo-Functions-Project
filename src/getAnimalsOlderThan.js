3fec483a82588ce768cfd5e1cd713a6b319251ea
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-05-23 14:08:13 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.filter(({ name }) => name === animal)
+    .map(({ residents }) => residents)
+    .every((idade) => idade[0].age > age);
 }
+
+/* Aqui eu primeiro filtrei ou separei o animal que é colocado como parâmetro dos outros animais da 'species', depois fiz um .map somente com os animais residentes dentro do paramêtro, por fim, fiz um every para verificar se cada um ou todos os animais possuem a idade mínima lançada no parâmetro.
+obs.: ( { name } => name ) ou ((elemento) => elemento.name) */
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
