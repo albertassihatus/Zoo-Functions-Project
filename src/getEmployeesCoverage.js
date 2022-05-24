dd7d085a222b4753582df1755bf982f566a00e0d
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-05-24 19:23:35 -0300

    requisitos 12 e 13

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage() {
-  // seu código aqui
 }
-
+console.log(getEmployeesCoverage(({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' })));
 module.exports = getEmployeesCoverage;
